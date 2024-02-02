396ce7f37031dcb38a39bebabaec914f3bf03ab0
Tej Prakash <120474834+52TEJ@users.noreply.github.com>
2024-02-02 05:04:29 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert (3 + 3) // we are using nodeJs, not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+console.log("Tejz")
+
+let name = "Tej"
+let age = 24
+let isLoggedIn = false
+let state;
+
+        //DATATYPES
+// number => 2 to the power of 53
+// bigint => only useful for bigger value than number
+// string => ""
+// boolean => true/false
+// null => standalone value (Empty)
+// undefined => not defined
+// symbol => unique
+
+        //OBJECTS
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
