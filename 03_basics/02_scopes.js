260a79029222f80c2b2a6d6980cae2d786dd55d4
Tej Prakash <120474834+52TEJ@users.noreply.github.com>
2024-02-12 13:30:54 +0000

    scopes in detail

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,11 +5,53 @@ if (true) {
     let a = 10
     const b = 20
     var c = 30
-    console.log("INNER: ", a);
+    // console.log("INNER: ", a);
 }
 
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one(){
+    const username = "Tejz"
+
+    function two(){
+        const website = "Youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+
+// one()
+
+if (true){
+    const username = "Tejz"
+    if(username === "Tejz"){
+        const website = " Youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// +++++++++++++++++++++++++++++++ interesting +++++++++++++++++++++++++++++++++++
+
+
+console.log(addOne(5));
+function addOne(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
