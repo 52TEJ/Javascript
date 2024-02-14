a9eba4ae1eee48760fc02af94c725735e99418e9
Tej Prakash <120474834+52TEJ@users.noreply.github.com>
2024-02-14 12:15:04 +0000

    for in

---
+++ 05_iterations/four.js
@@ -0,0 +1,27 @@
+const myObj = {
+    js: 'Javascript',
+    cpp: 'C++',
+    rb: 'Ruby',
+    swift: 'Swift by Apple'
+}
+
+for (const key in myObj) {
+    // console.log(`${key} shortcut is for ${myObj[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    // console.log(programming[key]);
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United State of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
+
